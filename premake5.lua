67f07b9b5fd43f4bc188ef9abcb0b8d134ca17dd
xMajedz <xmajedz@gmail.com>
2025-06-30 16:48:29 +0300

    Premake as a build system for tobas

---
+++ premake5.lua
@@ -0,0 +1,60 @@
+workspace "tobas"
+	configurations { "Debug", "Release" }
+
+project "tobas"
+	kind "WindowedApp"
+	language "C++"
+	targetdir "build"
+
+	includedirs {
+		"luau/Compiler/include",
+		"luau/VM/include",
+
+		"raylib/src",
+		"ode/include",
+
+		"sources",
+	}
+
+	libdirs {
+		"luau/build/release",
+
+		"raylib/src",
+		"ode/ode/src/.libs",
+	}
+
+	links {
+
+		"luaucompiler",
+		"luauast",
+		"luauvm",
+
+		"raylib",
+		"ode",
+	}
+
+	files {
+		"sources/luau.h", "sources/luau.cpp",
+
+		"sources/api.h", "sources/api.cpp",
+		"sources/api_game.h", "sources/api_game.cpp",
+		"sources/api_callbacks.h", "sources/api_callbacks.cpp",
+		"sources/api_raylib.h", "sources/api_raylib.cpp",
+		"sources/api_raymath.h", "sources/api_raymath.cpp",
+
+		"sources/game.h", "sources/game.cpp",
+		"sources/camera.h", "sources/camera.cpp",
+		"sources/player.h", "sources/player.cpp",
+		"sources/joint.h", "sources/joint.cpp",
+		"sources/body.h", "sources/body.cpp",
+
+		"sources/main.cpp",
+	}
+
+	filter { "configurations:Debug" }
+		defines { "DEBUG" }
+		symbols "On"
+
+	filter { "configurations:Release" }
+		defines { "NDEBUG" }
+		optimize "On"
