b30c99eb7b5f278ff19f093df761b4c23b893f86
xMajedz <xmajedz@gmail.com>
2025-07-16 19:53:51 +0300

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,86 +1,27 @@
-newaction {
-	trigger = "dist",
-	description = "distribute a release",
-	onStart = function() 
-		os.mkdir "dist"
-	end,
-	onProject = function(prj)
-		os.execute("zip dist/"
-			.. prj.name  .. "-" .. (_OPTIONS["release"] or "git")
-			.. ".zip mods scripts replays build/" .. prj.name
-		)
-	end,
-	onEnd = function() 
-		os.execute "ls dist"
-	end,
-}
-
-newaction {
-	trigger = "dist-clean",
-	description = "clean dist",
-	onStart = function() 
-		os.rmdir "dist"
-	end,
-}
-
-newaction {
-	trigger = "run",
-	description = "build and run source",
-	onStart = function() 
-		os.execute "make -j8 -C build"
-	end,
-	onEnd = function()
-		os.execute "build/tobas_sp"
-	end,
-}
-
-newaction {
-	trigger = "build",
-	description = "build source",
-	onStart = function() 
-		os.execute "make -j8 -C build"
-	end,
-}
-
-newaction {
-	trigger = "clean",
-	description = "clean",
-	onStart = function() 
-		os.rmdir "build"
-	end,
-}
+require "vendor/premake-ninja/ninja"
 
 workspace "tobas"
 	language "C++"
 	cppdialect "C++20"
 	configurations { "Debug", "Release" }
-	basedir "build"
-
-	includedirs {
-		"luau/Compiler/include", "luau/VM/include",
-
-		"raylib/src", "raygui/src",
-
-		"ode/include",
-		"enet/include",
-
-		"sources",
-	}
-
-	libdirs {
-		"luau/build/release",
-
-		"raylib/src",
-		--"ode/ode/src/.libs",
-		"ode/lib/Release",
-	}
-
+	location "build"
 	links {
-
 		"luaucompiler", "luauast", "luauvm",
-
 		"raylib",
 		"ode",
+	}
+
+	includedirs {
+		"vendor/luau/Compiler/include",
+		"vendor/luau/VM/include",
+
+		"vendor/raylib/src",
+		"vendor/raygui/src",
+
+		"vendor/ode/include",
+		"vendor/enet/include",
+
+		"sources",
 	}
 
 	files {
@@ -101,30 +42,28 @@ workspace "tobas"
 		"sources/body.h", "sources/body.cpp",
 	}
 
-	filter { "system:windows" }
-		links {"winmm", "gdi32", "opengl32"}
-
 	filter { "configurations:Debug" }
-		defines { "DEBUG" }
-		symbols "On"
-
+	defines { "DEBUG" }
+	symbols "On"
+	
 	filter { "configurations:Release" }
-		defines { "NDEBUG" }
-		optimize "On"
+	defines { "NDEBUG" }
+	optimize "On"
+
+	filter { "system:Windows" }
+	links {"winmm", "gdi32", "opengl32"}
+	libdirs { "lib/windows" }
+
+	filter { "not system:Windows" }
+	libdirs { "lib/linux" }
 
 project "tobas_sp"
 	kind "WindowedApp"
-	objdir "build"
-	basedir "build"
-	targetdir "build"
 	files { "sources/tobas_sp.cpp" }
 	defines { "OFFLINE" }
 
 project "tobas"
 	kind "WindowedApp"
-	objdir "build"
-	basedir "build"
-	targetdir "build"
 	files {
 		"sources/netcode_common.h", "sources/netcode_common.cpp",
 		"sources/netcode_server.h", "sources/netcode_server.cpp",
@@ -134,15 +73,8 @@ project "tobas"
 
 project "tobas_server"
 	kind "ConsoleApp"
-	objdir "build"
-	basedir "build"
-	targetdir "build"
 	files {
 		"sources/netcode_common.h", "sources/netcode_common.cpp",
 		"sources/netcode_server.h", "sources/netcode_server.cpp",
 		"sources/tobas_server.cpp",
 	}
-
-
-
-
